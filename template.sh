c68061478a2313f966317314da9357e5d1b2cf0d
Harini Mohankumar <mharinimohan1997@gmail.com>
2026-01-05 13:36:20 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,15 @@
+#Create directory
+mkdir -p src
+mkdir -p research
+#creating files
+touch src/__init__.py
+touch src/main.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.py
+touch requirements.txt
+
+
+echo "Project structure created."
